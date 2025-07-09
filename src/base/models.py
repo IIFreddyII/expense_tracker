dfaf45e2a6ebc488235cc68c3711fe30b137d8da
Alfredo Luna <alfredo.luna@s4learning.com>
2025-07-09 13:45:35 -0600

    Introduces base model for other models
    Adds an abstract base model with common fields
    (id, created_at, updated_at) to be inherited by other
    models in the system. This promotes code reuse and
    ensures consistency across models.

---
+++ src/base/models.py
@@ -0,0 +1,16 @@
+import uuid
+
+from django.db import models
+
+
+class BaseModel(models.Model):
+    """
+    Base model for create other system models.
+    """
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True

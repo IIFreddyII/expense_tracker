5ce0447e426746d297870bf6d2e9ba3f55a0dcff
Alfredo Luna <alfredo.luna@s4learning.com>
2025-07-10 12:51:28 -0600

    Adds logout functionality to the application
    Implements a logout feature, allowing users to securely end their sessions.
    
    This includes:
    - A "Cerrar Sesión" button in the header for authenticated users.
    - Associated URL and view to handle the logout request.
    - Basic styling for the logout button.

--- src/apps/finance/urls.py
+++ src/apps/finance/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from .views import DashboardView
 
 urlpatterns = [
     path("", DashboardView.as_view(), name="dashboard"),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
